82f7d1f155dcbe2b2ea6cc5e5c2a4b2054b6990e
ISTC\ITSC <uitaek111@gmail.com>
2025-02-24 14:24:52 +0900

    posts/models 에 있던 place 모델을 수정 후 seoul/models 로 이동
    이동에 따른 place 완료 기능 모델 수정
    place_image model 삭제 후 place의 imagefield로 수정
    
    posts/forms 에서 Postform에 title 추가
    commentform에 post 삭제

--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     user = models.ForeignKey("users.User",
                              verbose_name = "작성자",
                              on_delete = models.CASCADE)#유저 외래키
+    place = models.ManyToManyField("장소", related_name='related_posts')
     title = models.CharField("제목", max_length= 30)
-    complete = models.BooleanField("완료 여부", default=False)
     content = models.TextField("내용")
     created = models.DateTimeField("작성일시", auto_now_add = True)
     updated = models.DateTimeField("수정일시", auto_now=True)
@@ -14,11 +14,20 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class PlaceComplete(models.Model):
+    user = models.ForeignKey("users.User",
+                              on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    place = models.ForeignKey("seoul.Place", on_delete=models.CASCADE)
+    complete = models.BooleanField(default=False)
+
 class PostImage(models.Model):
     post = models.ForeignKey(Post,
                              verbose_name = "포스트",
                              on_delete = models.CASCADE)
     photo = models.ImageField("사진", upload_to = "post")
+ 
 
 class Comment(models.Model):
     user = models.ForeignKey("users.User",
--- seoul/models.py
+++ seoul/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name= models.CharField(max_length=100)
+    district = models.CharField(max_length=10)
+    place_image = models.ImageField(upload_to='place_images/')
+    address= models.TextField("주소")
