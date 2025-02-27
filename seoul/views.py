63f78353f6bb1ec4ae1e495e1cb3589ec8016e0e
Cho YoungHyun <mike135@naver.com>
2025-02-27 11:07:16 +0900

    서울 앱 html, view 수정
    html 표시 및  paginater 적용/ 상세 보기 페이지 생성

--- seoul/views.py
+++ seoul/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from seoul.models import Place
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 # Create your views here.
 def place_list(request):
     places = Place.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(places,8)
+
+    page_obj = paginator.get_page(page)
+
     context = {
         "places": places,
+        "page_obj":page_obj,
+        "paginator":paginator,
     }
     return render(request, "places.html", context)
 
 def place_detail(request, place_id):
-    places = get_object_or_404(Place, id=place_id)
+    place = get_object_or_404(Place, id=place_id)
     context = {
-        "places": places,
+        "place": place,
     }
     return render(request, "place_detail.html", context)
